78bacadb0939f4048a33446f93578626deacace8
Andres Bernardi <cito.bernardi@gmail.com>
2022-08-04 13:22:39 +0100

    adds liberte logo for admin

--- config/initializers/spree.rb
+++ config/initializers/spree.rb
@@ -49,7 +49,7 @@ Spree.config do |config|
   # Admin:
 
   # Custom logo for the admin
-  # config.admin_interface_logo = "logo/solidus.svg"
+  config.admin_interface_logo = "logo/liberte.png"
 
   # Gateway credentials can be configured statically here and referenced from
   # the admin. They can also be fully configured from the admin.
