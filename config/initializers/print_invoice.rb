5e478d707880471c6375615fb05887b6f4b6c9c5
andre <abernardi@geartranslations.com>
2022-09-30 16:52:09 +0100

    adds invoice number incremental

--- config/initializers/print_invoice.rb
+++ config/initializers/print_invoice.rb
@@ -1,2 +1,3 @@
-Spree::PrintInvoice::Config.set(print_invoice_logo_path: "logo/liberte.png")
-Spree::PrintInvoice::Config.set(print_buttons: "invoice")
+Spree::PrintInvoice::Config.set(print_invoice_logo_path: 'logo/liberte.png')
+Spree::PrintInvoice::Config.set(print_buttons: 'invoice')
+Spree::PrintInvoice::Config.set(print_invoice_next_number: 1)
